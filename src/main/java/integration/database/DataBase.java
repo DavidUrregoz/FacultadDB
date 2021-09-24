1201d96c79547d4eef0f2ffe1fa872b29ef9869c
DavidUrregoz <73442281+DavidUrregoz@users.noreply.github.com>
2021-09-24 14:23:01 -0500

    Interaccion de la 1 a la 6 funcionales

--- src/main/java/integration/database/DataBase.java
+++ src/main/java/integration/database/DataBase.java
@@ -14,4 +14,5 @@ public interface DataBase {
     public void close();
 
     public void printResultSet() throws SQLException;
+
 }
--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -3,35 +3,100 @@ package main;
 import integration.database.mysql.MySqlOperations;
 import org.apache.log4j.PropertyConfigurator;
 import java.sql.SQLException;
+import java.util.Scanner;
+
 import static util.enums.Log4jValues.LOG4J_PROPERTIES_FILE_PATH;
 import static util.enums.SystemProperties.USER_DIR;
 
 public class Main {
 
     private static final String SERVER = "sofka-training.cpxphmd1h1ok.us-east-1.rds.amazonaws.com";
-    private static final String DATA_BASE_NAME = "ivanArroyave_cine_10092021";
+    private static final String DATA_BASE_NAME = "davidUrregoZapata_facultad_10092021";
     private static final String USER = "sofka_training";
     private static final String PASSWORD = "BZenX643bQHw";
 
-    private static final String SELECT_ALL_FROM_CIUDAD = String.format("select * from %s.ciudad", DATA_BASE_NAME);
-    private static final String SELECT_ALL_FROM_DIRECTOR= String.format("select * from %s.director", DATA_BASE_NAME);
-    private static final String CALL_SP_LISTAR_PELICULAS= String.format("call sp_listar_peliculas()");
+    private static  String SELECT_ALL;
+    private static  String CALL_SP;
 
     private static final MySqlOperations mySqlOperations = new MySqlOperations();
-
+    public Scanner sca;
     public static void main(String[] args) throws Exception {
         PropertyConfigurator.configure(USER_DIR.getValue() + LOG4J_PROPERTIES_FILE_PATH.getValue());
-
+        Scanner sc2=new Scanner(System.in);
         login();
-        selectAllFromCiudad();
-        System.out.println("*********************************************");
-        System.out.println("*********************************************");
-        selectAllFromDirector();
-        System.out.println("*********************************************");
-        System.out.println("*********************************************");
-        callSpListarPeliculas();
+        boolean salida = true;
+        String entrada;
+        int eleccion;
+        while (salida){
+            System.out.println("Menu \n" +
+                    "1 ->Lista completa de Personas \n" +
+                    "2 ->Lista completa de Materias \n" +
+                    "3 ->Vista de Estudiantes Activos\n" +
+                    "4 ->Vista de Profesores Activos\n" +
+                    "5 ->Materias Disponibles y Horarios\n" +
+                    "6 ->Lugares de Recidencia \n" +
+                    "7 ->Busqueda De personas (cedula,nombre,ciudad,edad1,edad2) \n" +
+                    "8 ->Ingresar Persona (cedula,apellido,apellido,fecha de nacimiento,ciudad,esprofesor,estudiante)\n" +
+                    "9 ->Ingresar Materia (salon, bloque,nombre de materia,creditos,dias,horas,presencial,laboratorio)\n" +
+                    "10->Matricular estudiante a materia \n" +
+                    "11->Asignar profesor a Materia\n" +
+                    "12->Eliminacion logica de Persona \n" +
+                    "13->Inhabilitar materia \n" +
+                    "14->Asignar una nota de una amteria a un estudiante\n" +
+                    "16->Profesores, Asignaturas y  Horarios\n" +
+                    "17->Estudiantes y materias Matriculadas\n" +
+                    "0 ->SALIR");
+            entrada = sc2.next();
+            if (comprobarNumero(entrada)) {
+                eleccion = Integer.parseInt(entrada);
+            } else {
+                System.out.println("Ese no es un numero ");
+                continue;
+            }
+            switch (eleccion){
+                case 0:
+                    salida=false;
+                    break;
+                case 1:
+                    selectAllFromPersona();
+                    break;
+                case 2:
+                    selectAllFromMateria();
+                    break;
+                case 3:
+                    selectVistaEstudiantes();
+                    break;
+                case 4:
+                    selectVistaProfesores();
+                    break;
+                case 5:
+                    selectVistaMaterias();
+                    break;
+                case 6:
+                    spLugaresRecidencia();
+                    break;
+
+
+
+            }
+        }
         logout();
 
+
+        //selectAllFromDirector();
+
+        //callSpListarPeliculas();
+
+
+
+    }
+
+
+
+    public static boolean comprobarNumero(String n){
+        if(n.matches("[+-]?\\d*(\\.\\d+)?"))
+            return true;
+        return false;
     }
 
     private static void login(){
@@ -41,23 +106,59 @@ public class Main {
         mySqlOperations.setPassword(PASSWORD);
     }
 
-    private static void selectAllFromCiudad() throws SQLException {
-        mySqlOperations.setSqlStatement(SELECT_ALL_FROM_CIUDAD);
+    private static void selectAllFromPersona() throws SQLException {
+        alterarSeleccionar("persona");
+        mySqlOperations.setSqlStatement(SELECT_ALL);
+        sentecia();
+    }
+
+    private static void selectAllFromMateria() throws SQLException {
+        alterarSeleccionar("materia");
+        mySqlOperations.setSqlStatement(SELECT_ALL);
+        sentecia();
+    }
+
+    private static void selectVistaEstudiantes() throws SQLException {
+        alterarSeleccionar("vista_estudiantes");
+        mySqlOperations.setSqlStatement(SELECT_ALL);
+        sentecia();
+    }
+
+    private static void selectVistaProfesores() throws SQLException {
+        alterarSeleccionar("vista_esprofesores");
+        mySqlOperations.setSqlStatement(SELECT_ALL);
+        sentecia();
+    }
+
+    private static void selectVistaMaterias() throws SQLException {
+        alterarSeleccionar("materias_disponibles");
+        mySqlOperations.setSqlStatement(SELECT_ALL);
+        sentecia();
+    }
+
+    private static void spLugaresRecidencia() throws SQLException {
+        alterarProcedimientos("sp_listar_personas()");
+        mySqlOperations.setSqlStatement(CALL_SP);
+        sentecia();
+    }
+
+
+
+
+
+    private static void sentecia() throws SQLException{
         mySqlOperations.executeSqlStatement();
         mySqlOperations.printResultSet();
     }
 
-    private static void selectAllFromDirector() throws SQLException {
-        mySqlOperations.setSqlStatement(SELECT_ALL_FROM_DIRECTOR);
-        mySqlOperations.executeSqlStatement();
-        mySqlOperations.printResultSet();
+    private static void alterarSeleccionar(String vista){
+        SELECT_ALL = String.format("select * from %s.%s", DATA_BASE_NAME,vista);
     }
 
-    private static void callSpListarPeliculas() throws SQLException {
-        mySqlOperations.setSqlStatement(CALL_SP_LISTAR_PELICULAS);
-        mySqlOperations.executeSqlStatement();
-        mySqlOperations.printResultSet();
+    private static void alterarProcedimientos(String ps){
+        CALL_SP = String.format("call %s ",ps);
     }
+
 
     private static void logout(){
         mySqlOperations.close();
